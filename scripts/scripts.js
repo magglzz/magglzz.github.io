9bf10b7106aef7f2ee9aae304f020afd43d4c475
magglzz <margaretliu15@gmail.com>
2017-07-17 16:25:29 -0700

    color changes
    color scheme changes, contact page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,8 @@
+$('document').ready(function(){
 //accordian program
 function showAnswerRole() {
   $('#answer-role').slideToggle();
+  $(this).toggleClass('open');
 }
 
 function showAnswerGoal() {
@@ -13,11 +15,72 @@ $('#myrole').click(showAnswerRole);
 $('#goals').click(showAnswerGoal);
 
 //top nav 
-function showMenu() {
+  // $('#menu-btn').click(function() {
+  //   // Open menu
+  //   $('body').toggleClass('menu-open');
+  //   $('#menu-btn').toggleClass('active');
+  // });
+
+ $('#menu-btn').click(function() {
+    // Open menu
+    $('body').toggleClass('menu-open');
+    $('#menu-btn').toggleClass('active');
+  });
+/*function showMenu() {
   console.log('showmenu');
   $('.navigation').slideToggle();
+  $('#menu-btn').toggleClass('active');
 }
 
-// $('.navigation').hide();
+$('#menu-btn').click(showMenu);*/
 
-$('#menu-btn').click(showMenu);
+//see my work scroll
+
+  // listen for clicks on any anchor with
+  // a `data-action="jump-section"` attribute
+  $( 'a[data-action="jump-section"]' ).click(function(event) {
+    event.preventDefault();
+
+    // Find the href for the anchor
+    var namedAnchor = $(event.target).closest('a').attr('href');
+
+    if (namedAnchor) {
+      // Update the browser history
+      history.pushState({}, '', namedAnchor);
+      // Animate scroll to named anchor
+      $('body').animate({scrollTop: $(namedAnchor).offset().top-120}, 500);
+    }
+  });
+
+  // Initialize Slick carousel
+  console.log('slick loaded');
+  $('.carousel').slick({
+    dots: true,
+    // infinite: false,
+    speed: 300,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+ 
+});
+
